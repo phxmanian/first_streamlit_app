7fb399fdb4c17841b6383e724eb0e65c70ae4145
ManikandanK <112010429+phxmanian@users.noreply.github.com>
2022-08-24 15:28:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,7 @@ streamlit.text( '🌿 Veggies')
 streamlit.title('🍹 Create your Own Smoothie 🍹 ')
 
 import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
